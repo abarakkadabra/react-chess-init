12c1272e7f5f420ca6c3acfa7ab078ec2a300e96
wwwaw <aold88@gmail.com>
2022-07-19 22:04:00 +0300

    add available

--- src/components/CellComponent.tsx
+++ src/components/CellComponent.tsx
@@ -4,13 +4,17 @@ import { Cell } from '../models/Cell';
 interface CellProps {
     cell: Cell;
     selected: boolean;
-    click: (cell:Cell) => void;
+    click: (cell: Cell) => void;
 }
 
 const CellComponent: FC<CellProps> = ({ cell, selected, click }) => {
     return (
-        <div className={['cell', cell.color, selected ? 'selected' : ''].join(' ')}
-        onClick={()=>click(cell)}>
+        <div className={[
+            'cell', cell.color, selected ? 'selected' : '',
+            cell.available && cell.figure ? 'available' : '']
+            .join(' ')}
+            onClick={() => click(cell)}>
+            {cell.available && !cell.figure && <div className="available" />}
             {cell.figure?.logo && <img src={cell.figure.logo} alt="" />}
         </div>
     );
