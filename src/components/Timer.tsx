1303630349d71cd8884d00811f08dd5686562594
wwwaw <aold88@gmail.com>
2022-07-28 23:53:40 +0300

    timer and winner title

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -4,51 +4,51 @@ import { Player } from '../models/Player';
 
 interface TimerProps {
     currentPlayer: Player | null;
-    restart: ()=> void
+    restart: () => void
 }
 
 const Timer: FC<TimerProps> = ({ currentPlayer, restart }) => {
-    const [gameOver, setGameOver] = useState<boolean>(false)
     const [blackTime, setBlackTime] = useState(5)
     const [whiteTime, setWhiteTime] = useState(5)
     let timer = useRef<null | ReturnType<typeof setInterval>>(null);
-    
-    if((blackTime === 0 || whiteTime === 0) && !gameOver){
-        setGameOver(prev => !prev)
-    } 
+    const [winnerTitle, setWinnerTitle] = useState<string>('')
 
     useEffect(() => {
-        startTimer(gameOver)
-    }, [currentPlayer])
-    
-    function restartHandler(){
+
+        if (timer.current) {
+            clearInterval(timer.current)
+        }
+        // const callback = currentPlayer?.color === Colors.WHITE ? decrementWhiteTimer() : decrementBlackTimer()
+        timer.current = setInterval(() => {
+            if (currentPlayer?.color === Colors.WHITE) {
+                if (whiteTime !== 0) {
+                    setWhiteTime(prev => prev - 1)
+                } else {
+                    console.log('Times up! Black won!')
+                    setWinnerTitle('Times up! Black won!')
+                }
+            } else {
+                if (blackTime !== 0) {
+                    setBlackTime(prev => prev - 1)
+                } else {
+
+                    console.log('Times up! White won!')
+                    setWinnerTitle('Times up! White won!')
+                }
+            }
+        }, 1000)
+
+    }, [currentPlayer, blackTime, whiteTime, winnerTitle])
+
+    function restartHandler() {
         restart()
         setBlackTime(5)
         setWhiteTime(5)
-        setGameOver(false)
     }
-
-    const startTimer = (gameOver:boolean) => {
-            if(timer.current){
-                clearInterval(timer.current)
-            }
-            // const callback = currentPlayer?.color === Colors.WHITE ? decrementWhiteTimer() : decrementBlackTimer()
-            timer.current = setInterval((gameOver)=>{
-                if (currentPlayer?.color === Colors.WHITE){
-                    if(!gameOver){
-                        setWhiteTime(prev => prev-1)
-                    }
-                } else{
-                    console.log(gameOver)
-                    if(!gameOver){
-                        setBlackTime(prev => prev-1)
-                    }
-                }
-            }, 1000)
-        }
 
     return (
         <div className='timer'>
+            <h1>{winnerTitle}</h1>
             <h2>Black - {blackTime}</h2>
             <button className='myButton' onClick={restartHandler}>Restart</button>
             <h2>White - {whiteTime}</h2>
