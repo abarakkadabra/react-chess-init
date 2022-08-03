d5db50d3b1d389a2402a7ec820c996ce7b297891
wwwaw <aold88@gmail.com>
2022-08-04 01:23:24 +0300

    check

--- src/models/Cell.ts
+++ src/models/Cell.ts
@@ -20,106 +20,84 @@ export class Cell {
         this.available = false;
 
     }
-    setFigure(figure:Figure){
-        this.figure=figure;
+    setFigure(figure: Figure) {
+        this.figure = figure;
         this.figure.cell = this;
     }
-    moveFigure (target: Cell){
-        if (this.figure && this.figure?.canMove(target)){
+    moveFigure(target: Cell) {
+        if (this.figure && this.figure?.canMove(target)) {
             this.figure.moveFigure(target);
-            if(target.figure?.color === Colors.WHITE){
+            if (target.figure?.color === Colors.WHITE) {
                 this.board.lostWhiteFigures.push(target.figure)
             }
-            if(target.figure?.color === Colors.BLACK){
+            if (target.figure?.color === Colors.BLACK) {
                 this.board.lostBlackFigures.push(target.figure)
             }
             target.setFigure(this.figure);
-
-            
-
-            this.isKingUnderAttack(this.figure)
+            console.log(this.figure.color)
+            this.isKingUnderAttack(this.figure.color)
             this.figure = null;
         }
     }
-    
-    isKingUnderAttack(figure:Figure|null){
-        // let enemyKingCell: number[] = [];
-        let enemyKingCell:Cell;
-        
-        this.board.cells.forEach(cellRow=>{
-            cellRow.forEach(cell=>{
-                console.log(figure)
-                if(cell.figure?.name===FigureNames.KING && figure?.color !== cell.figure.color){
+    isKingUnderAttack(color: Colors) {
+        let enemyKingCell: Cell;
+        this.board.cells.forEach(cellRow => {
+            cellRow.forEach(cell => {
+                if (cell.figure?.name === FigureNames.KING && color !== cell.figure.color) {
                     enemyKingCell = cell
-                    
                 }
-                if(cell.figure !== undefined && enemyKingCell!==undefined){                
-                    
-                // console.log('x: ' + curCell.x + 'y: ' + curCell.y + 'colors: ' + this.figure?.color + curCell.figure.color)
-                    // enemyKingCell.push(curCell.x);
-                    // enemyKingCell.push(curCell.y);
-                    // enemyKingCell = this.board.getCell(curCell.x,curCell.y,)
-                    
-                    // console.log(curCell)
-                    // console.log(this.figure?.canMove(curCell))
-                    
-                    // console.log('isKingUnderAttack cell.figure ' )
-                    // console.log(cell.figure)  
-                    if(cell.figure?.canMove(enemyKingCell)){  
-                        
-                        // console.log(cell.figure?.color + 'CHECK!!')
+                if (cell.figure !== undefined && enemyKingCell !== undefined) {
+                    if (cell.figure?.canMove(enemyKingCell)) {
+                        console.log(cell.figure?.color + 'CHECK!!')
                     }
-                
                 }
             })
         })
-        
     }
-
-    isEmpty(){
+    isEmpty() {
         return this.figure === null;
     }
-    isEmptyVertical(target:Cell):boolean{
+    isEmptyVertical(target: Cell): boolean {
         if (this.x !== target.x) return false
-        const min = Math.min(this.y,target.y);
-        const max = Math.max(this.y,target.y); 
+        const min = Math.min(this.y, target.y);
+        const max = Math.max(this.y, target.y);
 
-        for(let y = min+1; y<max; y++){
-            if(!this.board.getCell(this.x,y).isEmpty()) return false
+        for (let y = min + 1; y < max; y++) {
+            if (!this.board.getCell(this.x, y).isEmpty()) return false
         }
 
         return true;
     }
 
-    isEmptyHorizontal(target:Cell):boolean{
+    isEmptyHorizontal(target: Cell): boolean {
         if (this.y !== target.y) return false
-        const min = Math.min(this.x,target.x);
-        const max = Math.max(this.x,target.x); 
+        const min = Math.min(this.x, target.x);
+        const max = Math.max(this.x, target.x);
 
-        for(let x = min+1; x<max; x++){
-            if(!this.board.getCell(x,this.y).isEmpty()) return false
+        for (let x = min + 1; x < max; x++) {
+            if (!this.board.getCell(x, this.y).isEmpty()) return false
         }
 
         return true;
     }
-    isEmptyDiagonal(target:Cell):boolean{
+    isEmptyDiagonal(target: Cell): boolean {
         const absY = Math.abs(target.y - this.y)
         const absX = Math.abs(target.x - this.x)
 
-        if(absX!==absY) return false
+        if (absX !== absY) return false
 
-        const dy = this.y < target.y ? 1:-1
-        const dx = this.x < target.x ? 1:-1
+        const dy = this.y < target.y ? 1 : -1
+        const dx = this.x < target.x ? 1 : -1
 
         for (let i = 1; i < absX; i++) {
-            if(!this.board.getCell(this.x + dx*i,this.y + dy*i).isEmpty()) return false
+            if (!this.board.getCell(this.x + dx * i, this.y + dy * i).isEmpty()) return false
         }
 
         return true;
     }
 
-    isEnemy(target:Cell){
-        if(target.figure){
+    isEnemy(target: Cell) {
+        if (target.figure) {
             return this.figure?.color !== target.figure?.color
         }
         return false
